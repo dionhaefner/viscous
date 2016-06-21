24518a813e996b4f20ebfb8898deee91a7406e7b
Dion Häfner <mail@dionhaefner.de>
2016-06-22 00:54:41 +0200

    added pgf plotting mode

--- viscous/__init__.py
+++ viscous/__init__.py
@@ -3,8 +3,6 @@ import os
 import copy
 import math
 
-_usetex = False
-
 import viscous.read
 import viscous.calc
 import viscous.utilities
@@ -15,36 +13,15 @@ sanitize = sanitize.sanitize
 figsize = figsize.figsize
 subplots = subplots.subplots
 
-def set_style(usetex=False):
-    global _usetex
-    _usetex = usetex
-    
+def set_style(out_type="screen"):
     sns.mpl.rcdefaults()
-    golden_ratio = (1 + math.sqrt(5))/2
+    golden_ratio = (math.sqrt(5) - 1)/2
     screen_width = 10.
-    paper_width = 4.786
-    if usetex: # assume we are producing printing quality plots
+    paper_width = 6.9
+    if out_type == "screen":
+        # produce screen-optimized plots
         rc_params = {
-                 "figure.figsize": (paper_width,paper_width/golden_ratio),
-                 "figure.dpi": 800/paper_width,
-                 "figure.titlesize": 12,
-                 "savefig.dpi": 300,
-                 "savefig.bbox": "standard",
-                 "savefig.format": "pdf",
-                 "font.family": "sans-serif",
-                 "font.serif": "Linux Libertine O",
-                 "font.sans-serif": "Fira Sans",
-                 "text.usetex": True,
-                 "text.latex.preamble": ["\\usepackage{gensymb}",
-                                         "\\usepackage{FiraSans}",
-                                         "\\usepackage{mathpazo}",
-                                         "\\usepackage{FiraMono}",
-                                         "\\usepackage[T1]{fontenc}"]
-                 }
-        font_scale = .5
-    else: # produce screen-optimized plots
-        rc_params = {
-                     "figure.figsize": (screen_width,screen_width/golden_ratio),
+                     "figure.figsize": (screen_width,screen_width*golden_ratio),
                      "figure.dpi": 800/screen_width,
                      "figure.titlesize": 24,
                      "savefig.dpi": 2*800/screen_width,
@@ -56,6 +33,68 @@ def set_style(usetex=False):
                      "lines.antialiased": True
                      }
         font_scale = 1.
+        context = "notebook"
+    elif out_type == "pdf":
+        # assume we are producing printing quality plots
+        rc_params = {
+                 "backend": "ps",
+                 "ps.papersize": "A4",
+                 "figure.figsize": (paper_width,paper_width*golden_ratio),
+                 "figure.titlesize": 11,
+                 "axes.titlesize": 10,
+                 "font.size": 10,
+                 "savefig.bbox": "standard",
+                 "savefig.format": "pdf",
+                 "font.family": "sans-serif",
+                 "font.serif": "Linux Libertine O",
+                 "font.sans-serif": "Fira Sans",
+                 "text.usetex": True,
+                 "text.latex.preamble": [
+					 "\\usepackage[T1]{fontenc}",
+					 "\\usepackage[utf8]{inputenc}",
+					 "\\usepackage{amsmath}",
+					 "\\usepackage{libertine}",
+                     "\\usepackage{FiraSans}",
+                     "\\usepackage{newtxmath}",
+                     "\\usepackage{FiraMono}"
+                    ]
+                 }
+        font_scale = 1.
+        context = "notebook"
+    elif out_type == "pgf":
+        # create pgf file for inclusion with pdflatex
+        rc_params = {
+                 "backend": "pgf",
+                 "figure.figsize": (paper_width,paper_width*golden_ratio),
+                 "figure.titlesize": 11,
+                 "axes.titlesize": 10,
+                 "axes.labelsize": 9,
+                 "font.size": 10,
+                 "savefig.bbox": "standard",
+                 "savefig.format": "pgf",
+                 "font.family": "sans-serif",
+                 "font.serif": [],
+                 "font.sans-serif": [],
+                 "text.usetex": True,
+                 "pgf.texsystem": "pdflatex",
+                 "pgf.preamble": [
+					 "\\usepackage[T1]{fontenc}",
+					 "\\usepackage[utf8]{inputenc}",
+					 "\\usepackage{amsmath}",
+					 "\\usepackage{libertine}",
+                     "\\usepackage{FiraSans}",
+                     "\\usepackage{newtxmath}",
+                     "\\usepackage{FiraMono}",
+                     "\\usepackage{microtype}",
+                     "\\usepackage{textcase}",
+                     "\\microtypesetup{expansion=false}",
+                     "\\newcommand{\\spacedallcaps}[1]{\\textls[160]{\\sffamily\\MakeTextUppercase{#1}}}"
+                    ]
+                 }
+        font_scale = 1.
+        context = "notebook"
+    else:
+        raise ValueError("Unrecognized output type: " + str(out_type))
 
-    sns.set(style="dark", context="notebook", font_scale=font_scale, rc=rc_params)
+    sns.set(style="dark", context=context, color_codes=True, font_scale=font_scale, rc=rc_params)
     sns.mpl.pyplot.close()
--- viscous/read.py
+++ viscous/read.py
@@ -5,7 +5,11 @@ import numpy as np
 
 from viscous.utilities import find_nearest
 
-CONSTANTS = ["days_in_norm_year", "grav", "omega", "radius", "cp_sw", "sound", "vonkar", "cp_air", "rho_air", "rho_sw", "rho_fw", "stefan_boltzmann", "latent_heat_vapor", "latent_heat_fusion", "ocn_ref_salinity", "sea_ice_salinity", "T0_Kelvin", "salt_to_ppt", "ppt_to_salt", "mass_to_Sv", "head_to_PW", "salt_to_Svppt", "salt_to_mmday", "momentum_factor", "hflux_factor", "salinity_factor", "sflux_factor", "nsurface_t", "nsurface_u"]
+CONSTANTS = ["days_in_norm_year", "grav", "omega", "radius", "cp_sw", "sound", "vonkar",
+"cp_air", "rho_air", "rho_sw", "rho_fw", "stefan_boltzmann", "latent_heat_vapor",
+"latent_heat_fusion", "ocn_ref_salinity", "sea_ice_salinity", "T0_Kelvin", "salt_to_ppt",
+"ppt_to_salt", "mass_to_Sv", "head_to_PW", "salt_to_Svppt", "salt_to_mmday", "momentum_factor",
+"hflux_factor", "salinity_factor", "sflux_factor", "nsurface_t", "nsurface_u"]
 
 def read_u_coords(path,return_dx=False):
     full_data = Dataset(path,"r")
@@ -38,7 +42,7 @@ def read_density(path):
     rho = np.array(full_data.variables["RHO"][0,...]) / 1E3 * 1E2**3 # g/cm^3 -> kg/m^3
     full_data.close()
     return rho
-    
+
 def read_potential_density(path):
     full_data = Dataset(path,"r")
     pd = np.array(full_data.variables["PD"][0,...]) / 1E3 * 1E2**3 # g/cm^3 -> kg/m^3
@@ -94,7 +98,7 @@ def read_temperature(path):
     temp = np.array(full_data.variables["TEMP"][0,...])
     full_data.close()
     return temp
-    
+
 def read_density_gradient(path):
     full_data = Dataset(path,"r")
     q = np.array(full_data.variables["Q"][0,...])/1E3*1E2**4 # g/cm^4 -> kg/m^4
@@ -106,13 +110,20 @@ def read_vorticity(path):
     pv = np.array(full_data.variables["PV"][0,...])*1E2 # 1/s/cm -> 1/s/m
     full_data.close()
     return pv
-    
+
+def read_windstress(path):
+    full_data = Dataset(path,"r")
+    taux = np.array(full_data.variables["TAUX"][0,...]) / (1E5 * 1E-2**2) # dyn/cm^2 -> N/m^2
+    tauy = np.array(full_data.variables["TAUY"][0,...]) / (1E5 * 1E-2**2) # dyn/cm^2 -> N/m^2
+    full_data.close()
+    return taux, tauy
+
 def read_constants(path):
     full_data = Dataset(path,"r")
     constants = {key: full_data.variables[key][0] for key in full_data.variables.keys() if key in CONSTANTS}
     full_data.close()
     return constants
-    
+
 def read_region_mask(path):
     full_data = Dataset(path,"r")
     region_mask = full_data.variables["REGION_MASK"][:]
@@ -121,5 +132,5 @@ def read_region_mask(path):
     regmask = np.ones(region_mask.shape, dict(names=regions, formats=[bool]*len(regions)))
     regmask['Pacific'] = (region_mask == 2)
     regmask['Atlantic'] = (region_mask >= 6) & (region_mask != 7)
-    regmask['Indo-Pacific'] = (region_mask >= 2) & (region_mask <= 3)    
+    regmask['Indo-Pacific'] = (region_mask >= 2) & (region_mask <= 3)
     return regmask
