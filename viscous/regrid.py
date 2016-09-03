575ba5924deca60e6d9ca77095327c0dd6fad7ab
Dion Häfner <mail@dionhaefner.de>
2016-09-03 02:25:53 +0200

    enhanced performance

--- viscous/regrid.py
+++ viscous/regrid.py
@@ -1,26 +1,46 @@
 import numpy as np
 from scipy import interpolate, spatial
 
-try:
-    from numba import jit
-except ImportError:
-    print("Numba not found")
-    jit = lambda f: f
-
 class Regridder(object):
     def __init__(self):
         self._points = None
         self._method = None
         self._nanmask = None
         self._triangulation = None
+        self._kdtree = None
+        self._dim = None
 
-    #@jit
+
+    def _nearest_interpolator(self,x,y):
+        xp = np.array([xi.flatten() for xi in x])
+        _, i = self._kdtree.query(xp.T, n_jobs=-1)
+        ival = y.flat[i].reshape(x[0].shape)
+        return np.squeeze(ival)
+
+    def _linear_interpolator(self,x,y):
+        if self._dim == 1:
+            ival = interpolate.interp1d(self._points.flatten(),
+                                        y, kind="linear")(x)[:,np.newaxis]
+        else:
+            ival = interpolate.LinearNDInterpolator(self._triangulation, y)(x)
+        return np.squeeze(ival)
+
+    def _cubic_interpolator(self,x,y):
+        if self._dim == 1:
+            ival = interpolate.interp1d(self._points[self._nanmask].flatten(),
+                                        y[self._nanmask].flatten(),
+                                        kind="cubic")(x)[:,np.newaxis]
+        else:
+            ival = interpolate.CloughTocher2DInterpolator(self._triangulation,
+                                                          y[self._nanmask])(x)
+        return np.squeeze(ival)
+
     def evaluate(self, points, values, rpoints, method="linear"):
-        points = np.squeeze(points)
-        if len(points.shape) == 1:
-            dim = 1
+        if len(np.array(points).shape) == 1:
+            self._dim = 1
+            points = points[:,np.newaxis]
         elif len(points.shape) == 2:
-            dim = points.shape[1]
+            self._dim = points.shape[1]
         else:
             raise ValueError("Points must be ndarray of shape (npoints,dim)")
 
@@ -28,14 +48,17 @@ class Regridder(object):
         if not method in methods:
             raise ValueError("Supported methods: {}".format(", ".join(methods)))
 
-        if method == "cubic" and dim > 2:
+        if method == "cubic" and self._dim > 2:
             raise ValueError("Cubic interpolator only supported for 1D and 2D data")
 
         nanmask = ~np.isnan(values)
         if not np.array_equal(self._points, points) \
           or self._method != method \
           or not np.array_equal(self._nanmask, nanmask):
-            if dim == 1 or method == "nearest":
+            self._kdtree = spatial.cKDTree(points)
+            if self._dim == 1:
+                self._triangulation = None
+            elif method == "nearest":
                 self._triangulation = None
             elif method == "linear":
                 self._triangulation = spatial.Delaunay(points)
@@ -45,29 +68,18 @@ class Regridder(object):
             self._method = method
             self._nanmask = nanmask
 
-        triangulation = self._triangulation
-        if dim == 1:
-            nearest_interpolator = lambda: interpolate.interp1d(points, values, kind="nearest")
-            linear_interpolator = lambda: interpolate.interp1d(points, values, kind="linear")
-            cubic_interpolator = lambda: interpolate.interp1d(points[nanmask], values[nanmask], kind="cubic")
-        else:
-            nearest_interpolator = lambda: interpolate.NearestNDInterpolator(points, values)
-            linear_interpolator = lambda: interpolate.LinearNDInterpolator(triangulation, values)
-            cubic_interpolator = lambda: interpolate.CloughTocher2DInterpolator(triangulation, values[nanmask])
-
         if method == "nearest":
-            ivalues = nearest_interpolator()(rpoints)
+            ivalues = self._nearest_interpolator(rpoints,values)
         elif method == "linear":
-            lval = linear_interpolator()(rpoints)
+            lval = self._linear_interpolator(rpoints,values)
             mask = np.isnan(lval)
-            lval[mask] = nearest_interpolator()(np.vstack([x[mask] for x in rpoints]).T)
+            lval[mask] = self._nearest_interpolator([x[mask] for x in rpoints],values)
             ivalues = lval
         elif method == "cubic":
-            cval = cubic_interpolator()(rpoints)
-            nvalues = nearest_interpolator()(rpoints)
-            cval[np.isnan(nvalues)] = np.nan
+            cval = self._cubic_interpolator(rpoints,values)
+            nval = self._nearest_interpolator(rpoints,values)
+            cval[np.isnan(nval)] = np.nan
             ivalues = cval
-
         return np.squeeze(ivalues)
 
 # global Regridder object so settings can be cached between function calls
