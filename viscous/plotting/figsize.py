9d7c958de489495524caad202b9812a488f23df9
Dion Häfner <mail@dionhaefner.de>
2016-09-23 14:12:46 +0200

    added screen figsize

--- viscous/plotting/figsize.py
+++ viscous/plotting/figsize.py
@@ -5,13 +5,15 @@ _TEXTWIDTH = 4.67
 _MARGINWIDTH = 1.80
 PHI = 1.618
 
-def figsize(n1=1,n2=1,fit="text"):
-    if fit == "page":
+def figsize(n1=1,n2=1,fit="screen"):
+    if fit == "screen":
+        width = viscous.mpl.rcParams["figure.figsize"][0]
+    elif fit == "page":
         width = _PAGEWIDTH
     elif fit == "text":
         width = _TEXTWIDTH
     elif fit == "margin":
         width = _MARGINWIDTH
     else:
-        raise ValueError("'fit' must be page, text, or margin")
+        raise ValueError("'fit' must be screen, page, text, or margin")
     return n1*width, n2*width/PHI
